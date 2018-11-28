ea5baaf4227b0f1f8872a9b76109aa929ad98fde
Yan Zhu <yanzhu@alauda.io>
2018-11-28 11:13:57 +0800

    try fix duplicate ingress rule

--- ingress/ingress.go
+++ ingress/ingress.go
@@ -19,6 +19,7 @@ package ingress
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/golang/glog"
@@ -113,9 +114,10 @@ func NewController(
 	eventBroadcaster.StartRecordingToSink(
 		&typedcorev1.EventSinkImpl{Interface: kubeclientset.CoreV1().Events("")},
 	)
+	hostname, _ := os.Hostname()
 	recorder := eventBroadcaster.NewRecorder(
 		scheme.Scheme,
-		corev1.EventSource{Component: "alb2", Host: config.Get("NAME")},
+		corev1.EventSource{Component: fmt.Sprintf("alb2-%s", config.Get("NAME")), Host: hostname},
 	)
 
 	controller := &Controller{
