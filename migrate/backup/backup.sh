93dfacbcd128110683ffd9e5ebfc233a01899401
cong <q1875486458@gmail.com>
2024-02-19 16:19:20 +0800

    update backup.sh

--- migrate/backup/backup.sh
+++ migrate/backup/backup.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
+function _clearup_cr() (
+  yq4 eval 'del(.metadata.ownerReferences,.metadata.uid,.metadata.resourceVersion,.metadata.generation,.metadata.annotations["kubectl.kubernetes.io/last-applied-configuration"],.status)' -
+)
+
+function _ignore_default_alb() (
+  grep -v 'cpaas-system' | grep -v 'global-alb2'
+)
+
 function backup_port_info() (
   echo "backup port info"
   while read -r cm; do
     echo "port info $cm"
-    kubectl get configmap -n cpaas-system $cm -o yaml >>./alb.backup.yaml
+    kubectl get configmap -n cpaas-system $cm -o yaml | _clearup_cr >>./alb.backup.yaml
     echo "---" >>./alb.backup.yaml
-  done < <(kubectl get configmap -n cpaas-system | grep -v global-alb2 | grep port-info | awk '{print $1}')
+  done < <(kubectl get configmap -n cpaas-system | _ignore_default_alb | grep port-info | awk '{print $1}')
 )
 
 function backup_user_rule() (
   echo "backup user created rule"
   while read -r rule; do
     echo "backup user created rule $rule"
-    kubectl get rule -n cpaas-system $rule -o yaml >>./alb.backup.yaml
+    kubectl get rule -n cpaas-system $rule -o yaml | _clearup_cr >>./alb.backup.yaml
     echo "---" >>./alb.backup.yaml
-  done < <(kubectl get rule -n cpaas-system -o=custom-columns='NAME:.metadata.name,SOURCE_TYPE:.spec.source.type' | grep -v ingress | tail -n +2 | awk '{print $1}')
+  done < <(kubectl get rule -n cpaas-system -o=custom-columns='NAME:.metadata.name,SOURCE_TYPE:.spec.source.type' | _ignore_default_alb | grep -v ingress | tail -n +2 | awk '{print $1}')
+)
+
+function _list_cert() (
+  (
+    kubectl get rule -n cpaas-system -o=custom-columns='NAME:.metadata.name,SOURCE_TYPE:.spec.certificate_name' | _ignore_default_alb | tail -n+2 | awk '{print $2}' | sort | uniq
+    kubectl get frontends.crd.alauda.io -n cpaas-system -o=custom-columns='NAME:.metadata.name,SOURCE_TYPE:.spec.certificate_name' | _ignore_default_alb | tail -n+2 | awk '{print $2}' | sort | uniq
+  ) | sort | uniq | grep -v 'none'
 )
 
 function backup_cert() {
@@ -27,18 +42,18 @@ function backup_cert() {
     local ns="${cert%%_*}"
     local secret="${cert#*_}"
     echo "backup cert $cert | $ns | $secret"
-    kubectl get secret -n $ns $secret -o yaml >>./alb.backup.yaml
+    kubectl get secret -n $ns $secret -o yaml | _clearup_cr >>./alb.backup.yaml
     echo "---" >>./alb.backup.yaml
-  done < <(kubectl get rule -n cpaas-system -o=custom-columns='NAME:.metadata.name,SOURCE_TYPE:.spec.certificate_name' | tail -n+2 | awk '{print $2}' | xargs | sort | uniq)
+  done < <(_list_cert)
 }
 
 function backup_ingress() (
   echo "backup user created ingress"
   while read -r ns name; do
     echo "ing $ns | $name"
-    kubectl get ingress -n $ns $name -o yaml >>./alb.backup.yaml
+    kubectl get ingress -n $ns $name -o yaml | _clearup_cr >>./alb.backup.yaml
     echo "---" >>./alb.backup.yaml
-  done < <(kubectl get ingress -A | grep -v 'global-alb2' | tail -n +2 | awk '{print $1,$2}')
+  done < <(kubectl get ingress -A | _ignore_default_alb | tail -n +2 | awk '{print $1,$2}')
 
 )
 
@@ -46,19 +61,24 @@ function backup_alb() (
   echo "backup user created alb"
   while read -r name; do
     echo "alb $name"
-    kubectl get alb2.v2beta1.crd.alauda.io -n cpaas-system $name -o yaml >>./alb.backup.yaml
+    kubectl get alb2.v2beta1.crd.alauda -n cpaas-system $name -o yaml | _clearup_cr >>./alb.backup.yaml
     echo "---" >>./alb.backup.yaml
-  done < <(kubectl get alb2.v2beta1.crd.alauda.io -n cpaas-system | tail -n +2 | awk '{print $1}' | grep -v global-alb2)
+  done < <(kubectl get alb2.v2beta1.crd.alauda -n cpaas-system | tail -n +2 | awk '{print $1}' | _ignore_default_alb)
 )
 
 function backup_ft() (
   echo "backup user created ft"
   while read -r name; do
     echo "ft $name"
-    kubectl get frontends.crd.alauda.io -n cpaas-system $name -o yaml >>./alb.backup.yaml
+    kubectl get frontends.crd.alauda.io -n cpaas-system $name -o yaml | _clearup_cr >>./alb.backup.yaml
     echo "---" >>./alb.backup.yaml
-  done < <(kubectl get frontends.crd.alauda.io -n cpaas-system | tail -n +2 | awk '{print $1}' | grep -v global-alb2)
+  done < <(kubectl get frontends.crd.alauda.io -n cpaas-system | tail -n +2 | awk '{print $1}' | _ignore_default_alb)
 )
+
+if ! command -v yq4 >/dev/null; then
+  echo "yq4 not found"
+  exit
+fi
 
 backup_alb
 backup_ft
