5a82a484037fc5ccc2fa5065c62729f4606b444a
cong <q1875486458@gmail.com>
2024-08-08 15:35:33 +0800

    test in github

--- scripts/run-like-ci-nginx.sh
+++ scripts/run-like-ci-nginx.sh
@@ -2,10 +2,9 @@
 # test current nginx
 # we should build a nginx image first then use this image to test.
 tag=$(yq '.global.images.nginx.tag' ./deploy/chart/alb/values.yaml)
-if [ ! "$1" = "" ]; then
-  tag="$1"
+image=build-harbor.alauda.cn/acp/alb2:$tag
+if [ -n "$1" ]; then
+  image="$1"
 fi
-image=build-harbor.alauda.cn/acp/alb-nginx:$tag
 # image=alb-nginx:test
-docker run --user root --network=host -e http_proxy=$HTTP_PROXY -e https_proxy=$HTTPS_PROXY -v $PWD:/acp-alb-test -it $image sh -c 'cd /acp-alb-test ;/acp-alb-test/scripts/nginx-test.sh'
-# docker run  --user root -it --network=host -e http_proxy=$HTTP_PROXY -e https_proxy=$HTTPS_PROXY -v $PWD:/acp-alb-test -it $image sh
+docker run --user root --network=host -v $PWD:/acp-alb-test -t $image sh -c 'cd /acp-alb-test ;/acp-alb-test/scripts/nginx-test.sh'
